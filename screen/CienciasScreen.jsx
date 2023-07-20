6624c7bf5596418643584e23f4cad464b520c9d0
Joshua08cr <alvaradojoshua499@gmail.com>
2023-07-20 07:44:49 -0600

    20/07/23

--- screen/CienciasScreen.jsx
+++ screen/CienciasScreen.jsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import react from "react";
+import React from "react";
+import { View, StyleSheet } from 'react-native'
+import UnityView from 'react-native-unity-view';
 
 const Ciencias = () => {
+    useEffect(() => {
+      UnityView.postMessage('LoadModel', '../components/celula.fbx');
+    }, []);
+
     return (
-      <View>
-        <Text>Ciencias</Text>
+      <View style={styles.container}> 
+        <UnityView style={styles.unityView}/>
       </View>
     )
   }
-  
+   
   export default Ciencias
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    unityView: {
+      flex: 1,
+    },
+  });
